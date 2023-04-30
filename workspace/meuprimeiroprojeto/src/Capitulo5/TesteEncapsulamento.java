97aa80fb2e32d823d9762aec42f091facee0c7b6
Felipe Carneiro <felipecarneiro@duck.com>
2023-04-29 23:26:49 -0300

    5.5. Encapsulamento e mod de acesso public/private

---
+++ workspace/meuprimeiroprojeto/src/Capitulo5/TesteEncapsulamento.java
@@ -0,0 +1,13 @@
+package Capitulo5;
+
+public class TesteEncapsulamento {
+    public static void main(String[] args) {
+        ArCondicionado ar = new ArCondicionado(); // 17 - 25
+
+        ar.trocarTemperatura(21);
+        System.out.println("Temperatura do ar: " + ar.obterTemperatura() + "ºC");
+
+        ar.trocarTemperatura(10);
+        System.out.println("Temperatura do ar: " + ar.obterTemperatura() + "ºC");
+    }
+}
