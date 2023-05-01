c19bf112eb8f8bb8b51617c53a065fe55228b78d
Felipe Carneiro <felipecarneiro@duck.com>
2023-04-30 22:59:43 -0300

    5.13. Desafio: pacotes e enumerações

---
+++ workspace/meuprimeiroprojeto/src/com/algaworks/cursojava/financeiro/modelo/Fornecedor.java
@@ -0,0 +1,13 @@
+package com.algaworks.cursojava.financeiro.modelo;
+
+public class Fornecedor {
+  private String nome;
+
+  public String getNome() {
+      return nome;
+  }
+
+  public void setNome(String nome) {
+      this.nome = nome;
+  }
+}
