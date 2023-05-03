72c332a226b7b8994669666d03063796e0e0557d
Felipe Carneiro <felipecarneiro@duck.com>
2023-05-03 01:07:29 -0300

    6.4. Tratando e lançando exceções

---
+++ workspace/meuprimeiroprojeto/src/com/algaworks/curso/fjoo/excecoes/SaldoInsuficienteException.java
@@ -0,0 +1,8 @@
+package com.algaworks.curso.fjoo.excecoes;
+
+public class SaldoInsuficienteException extends Exception{
+
+	public SaldoInsuficienteException(String mensagem) {
+		super(mensagem);
+	}
+}
