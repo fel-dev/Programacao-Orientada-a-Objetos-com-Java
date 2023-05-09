c1b48d264ee15f27caf4f98e74cfc763b38ff8aa
Felipe Carneiro <felipecarneiro@duck.com>
2023-05-09 10:36:13 -0300

    6.12. Métodos equals e hashCode

--- workspace/meuprimeiroprojeto/src/com/algaworks/curso/fjoo/collections/Aluno.java
+++ workspace/meuprimeiroprojeto/src/com/algaworks/curso/fjoo/collections/Aluno.java
@@ -23,4 +23,36 @@ public class Aluno {
 		this.idade = idade;
 	}
 
+
+
+	// tanto o hashcode quanto o equals são métodos para agilizar a busca de um objeto dentro de uma coleção
+	
+	// é um código que vai gerar um hash agrupando os objetos por esse hash q é unico
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+
+	// para dois alunos serem iguais, o nome deles tem que ser igual
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluno other = (Aluno) obj;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+
 }
