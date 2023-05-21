b6b475392e1fc84ad63b25dfa7d9da4fb18c9921
Felipe Carneiro <felipecarneiro@duck.com>
2023-05-21 15:05:44 -0300

    8.3. A classe Scanner

---
+++ workspace/meuprimeiroprojeto/src/com/algaworks/cap8/exemplo-oi/src/LendoArquivoItensPedido.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class LendoArquivoItensPedido {
+
+	public static void main(String[] args) {
+		try (Scanner scanner = new Scanner(new BufferedReader(new FileReader("itens_pedido.txt")))) {
+		scanner.useDelimiter(";");
+		Locale localeBrasil = new Locale("pt", "BR");
+		scanner.useLocale(localeBrasil);
+
+		NumberFormat formatador = NumberFormat.getCurrencyInstance(localeBrasil);
+		
+		while(scanner.hasNext()) {
+			String produto = scanner.next();
+			int quantidade = scanner.nextInt();
+			double valor = scanner.nextDouble();
+			scanner.nextLine();
+			
+			System.out.printf("Produto: %s, Quantidade: %d, Valor: %s\n", 
+							produto, quantidade, formatador.format(valor));
+		}
+		
+		} catch (IOException e) {
+			System.err.println("Erro ao ler arquivo: " + e.getMessage());
+		}
+
+	}
+
+}
