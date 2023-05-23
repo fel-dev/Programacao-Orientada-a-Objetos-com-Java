29b29a54be81a8b1e0a7db80e06ef8460e5e9418
Felipe Carneiro <felipecarneiro@duck.com>
2023-05-23 10:49:51 -0300

    9.2. Enviando objetos na rede começando com o cliente

---
+++ workspace/meuprimeiroprojeto/src/com/algaworks/cap9/chat-servidor/src/com/algaworks/chat/servidor/Servidor.java
@@ -0,0 +1,11 @@
+package com.algaworks.chat.servidor;
+
+import java.net.ServerSocket;
+
+public class Servidor {
+    public static void main(String[] args) {
+        try (ServerSocket server = new ServerSocket(3333)) {
+            
+        }
+    }
+}
