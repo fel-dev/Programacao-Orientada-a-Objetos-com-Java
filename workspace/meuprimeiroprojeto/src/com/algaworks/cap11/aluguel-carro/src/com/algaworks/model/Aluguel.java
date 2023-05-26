6dfb894a7f5273f05468387257cb3602845592b8
Felipe <correaflp@gmail.com>
2023-05-26 15:16:22 -0300

    11.8. API de Data - Parte 4

--- workspace/meuprimeiroprojeto/src/com/algaworks/cap11/aluguel-carro/src/com/algaworks/model/Aluguel.java
+++ workspace/meuprimeiroprojeto/src/com/algaworks/cap11/aluguel-carro/src/com/algaworks/model/Aluguel.java
@@ -1,5 +1,6 @@
 package com.algaworks.model;
 
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
@@ -10,13 +11,14 @@ public class Aluguel {
 
   private LocalDateTime dataEhoraRetirada;
   private LocalDateTime dataeHoraPrevistaDevolucao;
-  private LocalDate dataeHoraRealDevolucao;
+  private LocalDateTime dataEHoraRealDaDevolucao;
 
   public Aluguel(Cliente cliente, Carro carro, LocalDateTime dataEhoraRetirada, LocalDateTime dataeHoraPrevistaDevolucao) {
     this.cliente = cliente;
     this.carro = carro;
     this.dataEhoraRetirada = dataEhoraRetirada;
     this.dataeHoraPrevistaDevolucao = dataeHoraPrevistaDevolucao;
+
   }
 
   public Cliente getCliente() {
@@ -51,12 +53,24 @@ public class Aluguel {
     this.dataeHoraPrevistaDevolucao = dataeHoraPrevistaDevolucao;
   }
 
-  public LocalDate getDataeHoraRealDevolucao() {
-    return dataeHoraRealDevolucao;
+  public LocalDateTime setDataEHoraRealDaDevolucao() {
+    return dataEHoraRealDaDevolucao;
   }
 
-  public void setDataeHoraRealDevolucao(LocalDate dataeHoraRealDevolucao) {
-    this.dataeHoraRealDevolucao = dataeHoraRealDevolucao;
+  public void setDataEHoraRealDaDevolucao(
+        LocalDateTime dataEHoraRealDaDevolucao) {
+    this.dataEHoraRealDaDevolucao = dataEHoraRealDaDevolucao;
+  }     
+
+  public void imprimeFatura() {
+        Duration duracao = Duration.between(dataeHoraPrevistaDevolucao, dataEHoraRealDaDevolucao);
+        System.out.println(">>>> Fatura <<<<"); ;
+        if (!duracao.isNegative()) {
+                double multa = duracao.toHours() * carro.getValorDiaria() * 0.1;
+                System.out.printf("Valor da multa: R$ %.2f. Pois o carro ficou %d dias atrasado.\n", multa, duracao.toHours());
+        } else {
+                System.out.println("Obrigado pela caixinha! Volte sempre!!!");
+        }
   }
 
   
